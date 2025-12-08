c4eccd27c41c3c1e1c2a428200284d0287658736
Maarten van Nimwegen <maartenvannimwegen@hotmail.com>
2025-12-08 20:23:49 +0100

    Init UserService and repository

---
+++ Arclight.Application/Interfaces/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using Arclight.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arclight.Application.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetByIdAsync(Guid id);
+        Task AddAsync(User user);
+        Task SaveChangesAsync();
+    }
+}
---
+++ Arclight.Application/Services/UserService.cs
@@ -0,0 +1,31 @@
+﻿using Arclight.Application.Interfaces;
+using Arclight.Domain.Entities;
+using Arclight.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arclight.Application.Services
+{
+    public interface IUserService
+    {
+        Task<Guid> CreateUserAsync(string email, string firstName, string lastName, string passwordHash, UserRole role);
+        Task<User?> GetUserAsync(Guid id);
+    }
+
+    public class UserService(IUserRepository repository) : IUserService
+    {
+        public async Task<Guid> CreateUserAsync(string email, string firstName, string lastName, string passwordHash, UserRole role)
+        {
+            var user = new User(email, firstName, lastName, passwordHash, role);
+            await repository.AddAsync(user);
+            await repository.SaveChangesAsync();
+            return user.Id;
+        }
+
+        public async Task<User?> GetUserAsync(Guid id)
+        {
+            return await repository.GetByIdAsync(id);
+        }
+    }
+}
