abd94dbda1176622295b3d1a84b1d54cae634352
Maarten van Nimwegen <maartenvannimwegen@hotmail.com>
2026-03-20 11:27:29 +0100

    fix: fixed typo in update article request validator

--- Arclight.Application/Validations/UpdateArticleRequestValidator.cs
+++ Arclight.Application/Validations/UpdateArticleRequestValidator.cs
@@ -9,11 +9,11 @@ public class UpdateArticleRequestValidator : AbstractValidator<UpdateArticleRequ
     {
         RuleFor(x => x.Title)
             .NotEmpty().WithMessage("Title cannot be empty.")
-            .MaximumLength(100).WithMessage("Title may contain a maximal of 100 characters.");
+            .MaximumLength(100).WithMessage("Title may contain a maximum of 100 characters.");
 
         RuleFor(x => x.Summary)
             .NotEmpty().WithMessage("Summary cannot be empty.")
-            .MaximumLength(500).WithMessage("Summary is to long. Use a maximal of 500 characters.");
+            .MaximumLength(500).WithMessage("Summary is too long. Use a maximum of 500 characters.");
 
         RuleFor(x => x.Content)
             .NotEmpty().WithMessage("Content cannot be empty.");
